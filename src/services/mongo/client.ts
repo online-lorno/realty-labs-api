8aaf8ae435c8716ac511cefd52f2dca1e2688724
Leonard Laput <online.lorno@gmail.com>
2021-09-15 10:39:28 +0800

    refactor: update mongo client

--- src/services/mongo/client.ts
+++ src/services/mongo/client.ts
@@ -2,8 +2,10 @@ import mongoose from 'mongoose'
 
 import config from '@config/mongo'
 
-const client = async () => {
+const connect = async () => {
   await mongoose.connect(config.host, config?.options)
 }
 
-export default client
+export default {
+  connect,
+}
