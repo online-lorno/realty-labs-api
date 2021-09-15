9442f1d6bef3efc5070342bf63cdd6a7a0f0170c
Leonard Laput <online.lorno@gmail.com>
2021-09-15 10:13:30 +0800

    feat: user model

---
+++ src/models/User.ts
@@ -0,0 +1,58 @@
+import { Schema, model, Document } from 'mongoose'
+import md5 from 'md5'
+
+export enum UserType {
+  Broker = 0,
+  Agent = 1,
+}
+
+export enum UserLoginType {
+  email = 0,
+  facebook = 1,
+  google = 2,
+}
+
+export interface IUser extends Document {
+  email: string
+  name: string
+  phone: string
+  type: UserType
+}
+
+const UserSchema: Schema = new Schema(
+  {
+    // Main fields
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    type: { type: Number, enum: Object.values(UserType), required: true },
+
+    // Login fields
+    login_type: {
+      type: Number,
+      enum: Object.values(UserLoginType),
+      required: true,
+    },
+    password: { type: String },
+
+    // Optional fields
+    phone: { type: String },
+  },
+  {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+    collection: 'User',
+  }
+)
+
+// Middlewares
+UserSchema.pre('save', function (next) {
+  if (this.isModified('password')) {
+    this.password = md5(this.password)
+  }
+  next()
+})
+
+// Indexes
+UserSchema.index({ email: 1 })
+UserSchema.index({ email: 1, type: 1 })
+
+export default model<IUser>('User', UserSchema)
