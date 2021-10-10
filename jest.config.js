48edda804f92fc7b60585ef65d56005d46145375
Leonard Laput <online.lorno@gmail.com>
2021-10-10 18:35:18 +0800

    fix: jest paths

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['./'],
+  moduleNameMapper: {
+    '^@config(.*)$': '<rootDir>/src/config$1',
+    '^@constants(.*)$': '<rootDir>/src/constants$1',
+    '^@handlers(.*)$': '<rootDir>/src/handlers$1',
+    '^@libs(.*)$': '<rootDir>/src/libs$1',
+    '^@models(.*)$': '<rootDir>/src/models$1',
+    '^@models(.*)$': '<rootDir>/src/models$1',
+  },
+}
