82480d47fd79672a6c72c18e7c91855fddb336e1
Leonard Laput <online.lorno@gmail.com>
2021-10-10 20:33:13 +0800

    feat: update config for testing

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,11 +1,24 @@
 import { MongoDbConfigProps } from './types'
 
-const config: MongoDbConfigProps = {
-  host: process.env.MONGODB_URI || '',
-  options: {
-    dbName: process.env.MONGODB_DB || 'realty-labs-dev',
-    useNewUrlParser: true,
-  },
+const globalAny: any = global
+let config: MongoDbConfigProps
+
+if (process.env.NODE_ENV === 'test') {
+  config = {
+    host: globalAny.__MONGO_URI__,
+    options: {
+      dbName: globalAny.__MONGO_DB_NAME__,
+      useNewUrlParser: true,
+    },
+  }
+} else {
+  config = {
+    host: process.env.MONGODB_URI || '',
+    options: {
+      dbName: process.env.MONGODB_DB || 'realty-labs-dev',
+      useNewUrlParser: true,
+    },
+  }
 }
 
 export default config
